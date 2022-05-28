176751ea8bbac2f5b594da6c11555a6a1f8fa0a6
Henry Salazar <hjsmipuc@gmail.com>
2022-05-28 18:02:33 -0500

    Definiendo las tasas de cambio de cada una de las monedas y haciendo la conversion

--- src/com/alura/conversor/Principal.java
+++ src/com/alura/conversor/Principal.java
@@ -1,7 +1,5 @@
 package com.alura.conversor;
 
-
-
 import javax.swing.JOptionPane;
 
 public class Principal {
@@ -13,9 +11,7 @@ public class Principal {
 		
 		String mensaje1 = "Ingresa la cantidad de dinero que deseas convertir";
 		String mensaje2 = "Ingresa el valor de la temperatura que deseas convertir";
-		
 		String entrada;
-		
 
 		//Menu
 		Object[] opciones = { opcion1, opcion2 };
@@ -26,15 +22,16 @@ public class Principal {
 		System.out.println(itemSeleccionado);
 		
 		//Input
+		double valor = 0;
 		if (itemSeleccionado.equals(opcion1)) {
 			entrada = JOptionPane.showInputDialog(mensaje1);
-			double valor = Double.parseDouble(entrada);
+			valor = Double.parseDouble(entrada);
 			System.out.println(valor);
 		}
 		
 		if (itemSeleccionado.equals(opcion2)) {
 			entrada = JOptionPane.showInputDialog(mensaje2);
-			double valor = Double.parseDouble(entrada);
+			valor = Double.parseDouble(entrada);
 			System.out.println(valor);
 		}
 		
@@ -44,11 +41,79 @@ public class Principal {
 		String opcionSeleccionada = (String) JOptionPane.showInputDialog(null, "Elije la moneda a la que deseas convertir tu dinero"
 				+ "", "Monedas", JOptionPane.DEFAULT_OPTION,  null, posiblesMonedas, posiblesMonedas[0]);
 		
-		System.out.println(opcionSeleccionada);
+		double resultado = 0;
+		switch(opcionSeleccionada) {
+					
+			case "De Pesos a Dolar": {
+				System.out.println("Convierte a dolar");
+				resultado = 0.00026 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado+ "dolares");
+				break;
+			}
+			case "De Pesos a Euros": {
+				System.out.println("Convierte a euro");
+				resultado = 0.00024 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "euros");
+				break;
+			}
+			case "De Pesos a Libras": {
+				System.out.println("De Pesos a Libras");
+				resultado = 0.00020 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "libras");
+				break;
+			}
+			case "De Pesos a Yen": {
+				System.out.println("De Pesos a Yen");
+				resultado = 0.033 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "yen");
+				break;
+			}
+			case "De Pesos a Won coreano": {
+				System.out.println("De Pesos a Won coreano");
+				resultado = 0.32 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "won coreano");
+				break;
+			}
+			case "De Dolar a Pesos": {
+				System.out.println("De Dolar a Pesos");
+				resultado = 3922.50 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "pesos");
+				break;
+			}
+			case "De Euros a Pesos": {
+				System.out.println("De Euros a Pesos");
+				resultado = 4209.69 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "pesos");
+				break;
+			}
+			case "De libras a Pesos": {
+				System.out.println("De libras a Pesos");
+				resultado = 4953.76 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "pesos");
+				break;
+			}
+			case "De Yen a Pesos": {
+				System.out.println("De Yen a Pesos");
+				resultado = 30.85 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "pesos");
+				break;
+			}
+			case "De Won coreano a Pesos": {
+				System.out.println("De Won coreano a Pesos");
+				resultado = 3.12 * valor;
+				JOptionPane.showMessageDialog(null, "Tienes $" + resultado + "pesos");
+				break;
+			}
+					
+		}
 
 	}
 
 }
+
+
+
+
 
 
 
